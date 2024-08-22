47615c1571ab5eb850fa33d7fa2b954dffc1b50b
JakubCzuba <jakub.czuba.it@gmail.com>
2024-08-22 15:55:47 +0200

    entry working

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-import tkinter as tk 
+import tkinter as tk
 
 root = tk.Tk()
 root.geometry("800x500")
 root.title("First py")
-root.mainloop()
 
+def get_data():
+    data = myentry.get()
+    Label2.config(text= "Twoje dane to: " + data)
+
+Label = tk.Label(root, text = "Wprowadź dane poniżej", font=("Arial", 20))
+Label.pack(padx=20, pady=20) 
+
+myentry = tk.Entry(root)
+myentry.pack(padx=10, pady=10)
+
+button_get_data = tk.Button(root, text="Pobież", command=get_data)
+button_get_data.pack(padx=15, pady=15)
+
+Label2 = tk.Label()
+Label2.pack()
+
+
+
+root.mainloop()
