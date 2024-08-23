b9257cce76f629190870a27f198f3c74d9bdd970
JakubCzuba <jakub.czuba.it@gmail.com>
2024-08-23 11:21:05 +0200

    style

--- main.py
+++ main.py
@@ -8,16 +8,26 @@ def get_data():
     data = myentry.get()
     Label2.config(text= "Twoje dane to: " + data)
 
-Label = tk.Label(root, text = "Wprowadź dane poniżej", font=("Arial", 20))
+
+
+frametop = tk.Frame(root, bg="linen")
+frametop.pack(side="top", fill="x")
+
+Label = tk.Label(frametop, text = "Wprowadź dane poniżej", font=("Arial", 20), bg="linen")
 Label.pack(padx=20, pady=20) 
 
-myentry = tk.Entry(root)
-myentry.pack(padx=10, pady=10)
 
-button_get_data = tk.Button(root, text="Pobież", command=get_data)
+
+frameleft = tk.Frame(root, bg="powder blue")
+frameleft.pack(side="left", fill="y")
+
+button_get_data = tk.Button(root, text="Zatwierdź", command=get_data, bg="linen")
 button_get_data.pack(padx=15, pady=15)
 
-Label2 = tk.Label()
+myentry = tk.Entry(frameleft)
+myentry.pack(padx=10, pady=10)
+
+Label2 = tk.Label(frameleft, bg="powder blue")
 Label2.pack()
 
 
